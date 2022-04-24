4895eaa3d4d16e1836fb1174e0c86c6920c8b78d
ptprashanttripathi <ptprashanttripathi@outlook.com>
2022-04-24 14:41:44 +0530

    added sample

--- js/editor-console.js
+++ js/editor-console.js
@@ -1,42 +1,50 @@
-let console = (function (oldConsole) {
+let console = (function(oldConsole) {
     return {
-        formatArgsOutput: function(arg) { 
+        formatArgsOutput: function(arg) {
             let outputArgMessage;
 
             // Deal with different data types
             switch (this.getType(arg)) {
-                case "string":
-                    outputArgMessage = `"${arg}"`;
-                    break;
-                case "object":
-                    outputArgMessage = `Object ${JSON.stringify(arg)}`;
-                    break;
-                case "array":
-                    outputArgMessage = `Array ${JSON.stringify(arg)}`;
-                    break;
-                default:
-                    outputArgMessage = arg;
-                    break;
+            case "string":
+                outputArgMessage = `"${arg}"`;
+                break;
+            case "object":
+                outputArgMessage = `Object ${JSON.stringify(arg)}`;
+                break;
+            case "array":
+                outputArgMessage = `Array ${JSON.stringify(arg)}`;
+                break;
+            default:
+                outputArgMessage = arg;
+                break;
             }
 
             return outputArgMessage;
         },
-        getType: function (arg) {
-            if (typeof arg === "string") return "string";
-            if (typeof arg === "boolean") return "boolean";
-            if (typeof arg === "function") return "function";
-            if (typeof arg === "number") return "number";
-            if (typeof arg === "undefined") return "undefined";
-            if (typeof arg === "object" && !Array.isArray(arg)) return "object";
-            if (typeof arg === "object" && Array.isArray(arg)) return "array";
+        getType: function(arg) {
+            if (typeof arg === "string")
+                return "string";
+            if (typeof arg === "boolean")
+                return "boolean";
+            if (typeof arg === "function")
+                return "function";
+            if (typeof arg === "number")
+                return "number";
+            if (typeof arg === "undefined")
+                return "undefined";
+            if (typeof arg === "object" && !Array.isArray(arg))
+                return "object";
+            if (typeof arg === "object" && Array.isArray(arg))
+                return "array";
         },
-        logMultipleArguments: function (arguments) {
+        logMultipleArguments: function(arguments) {
             let currentLog = "";
 
             // Deal with multiple arguments
-            arguments.forEach(arg => {
+            arguments.forEach(arg=>{
                 currentLog += this.formatArgsOutput(arg) + " ";
-            });
+            }
+            );
 
             oldConsole.log.apply(oldConsole, arguments);
 
@@ -47,7 +55,7 @@ let console = (function (oldConsole) {
 
             oldConsole.log(consoleMessages);
         },
-        logSingleArgument: function (logItem) {
+        logSingleArgument: function(logItem) {
             oldConsole.log(logItem);
             consoleMessages.push({
                 message: this.formatArgsOutput(logItem),
@@ -56,17 +64,17 @@ let console = (function (oldConsole) {
 
             oldConsole.log(consoleMessages);
         },
-        log: function (text) {
+        log: function(text) {
             let argsArray = Array.from(arguments);
             return argsArray.length !== 1 ? this.logMultipleArguments(argsArray) : this.logSingleArgument(text);
         },
-        info: function (text) {
+        info: function(text) {
             oldConsole.info(text);
         },
-        warn: function (text) {
+        warn: function(text) {
             oldConsole.warn(text);
         },
-        error: function (err) {
+        error: function(err) {
             oldConsole.error(err);
             consoleMessages.push({
                 message: `${err.name}: ${err.message}`,
@@ -74,4 +82,5 @@ let console = (function (oldConsole) {
             });
         }
     }
-})(window.console);
+}
+)(window.console);
