c8128ea5e4265d1b1ddc1dd7a3058d7e1309d76a
Pt. Prashant tripathi <26687933+PtPrashantTripathi@users.noreply.github.com>
2022-10-12 11:20:24 -0400

    🔥 Hot fix - Vedic Version 1.2.2 (#1)
    * Add files via upload
    
    * Delete vedic.js
    
    * Add files via upload

--- js/editor.js
+++ js/editor.js
@@ -9,7 +9,7 @@ const toggleBtn = document.getElementById("btn-toggle");
 let editor = ace.edit("editor");
 let session = editor.session;
 let defaultCode =
-  '# We have inbuilt transliteration tool\n# your input will be translitered to sanskrit once you press spacebar or enter key\n\nमान आयु = ३२;\nयदि (आयु == २५) {\n  वद् "वयं सम वयस्काः एव"; # We\'re the same age\n}\nअथ यदि (आयु > २५) {\n  वद् "वयं सम वयस्काः न एव"; # We\'re not the same age\n}\nअथ {\n  वद् "मम अपेक्षया कनिष्ठः वा"; # Younger to me\n}';
+  '# We have inbuilt transliteration tool\n# your input will be translitered to sanskrit once you press spacebar or enter key\n\nमान आयु = ३२;\nयदि (आयु == २५) {\n  वद "वयं सम वयस्काः एव"; # We\'re the same age\n}\nअथ यदि (आयु > २५) {\n  वद "वयं सम वयस्काः न एव"; # We\'re not the same age\n}\nअथ {\n  वद "मम अपेक्षया कनिष्ठः वा"; # Younger to me\n}';
 let consoleMessages = [];
 
 let editorLib = {
@@ -47,7 +47,7 @@ let editorLib = {
           getCompletions: function (editor, session, pos, prefix, callback) {
             var kwList = [
               ["maana", "मान"],
-              ["vad", "वद्"],
+              ["vad", "वद"],
               ["yadi", "यदि"],
               ["atha", "अथ"],
               ["satya", "सत्य"],
@@ -113,7 +113,7 @@ resetCodeBtn.addEventListener("click", () => {
 editorLib.init();
 
 /*/add keyword button dnymically
-let Keyword = ["मान", "वद्", "यदि", "अथ", "सत्य", "असत्य", "सूत्र", "फल", "चक्रम्", "पर्यन्तम्", "विराम्", "निर्देश", "यद", "यदभावे", "अवहन्"];
+let Keyword = ["मान", "वद", "यदि", "अथ", "सत्य", "असत्य", "सूत्र", "फल", "चक्रम्", "पर्यन्तम्", "विराम्", "निर्देश", "यद", "यदभावे", "अवहन्"];
 let KeywordArea = document.getElementsByClassName("hint-area")[0];
 Keyword.forEach((keyword) => {
   let newBtn = document.createElement("button");
@@ -127,38 +127,38 @@ Keyword.forEach((keyword) => {
 
 // examples
 const examples = new Map([
-  ["hello-world.ved", `वद् "हेलो विश्व!";`],
+  ["hello-world.ved", `वद "हेलो विश्व!";`],
   [
     "operators.ved",
-    "वद् २+१; # ३\nवद् २-१; # १\nवद् २*१; # २\nवद् २/१; # २\nवद् २%१; # ०\nवद् २>१; # सत्य\nवद् २<१; # असत्य\nवद् २==१; # असत्य\nवद् २!=१; # सत्य\nवद् २>=१; # सत्य\nवद् २>=१; # सत्य\nवद् २<=१; # असत्य\nवद् २&&१; # सत्य\nवद् २||१; # सत्य",
+    "वद २+१; # ३\nवद २-१; # १\nवद २*१; # २\nवद २/१; # २\nवद २%१; # ०\nवद २>१; # सत्य\nवद २<१; # असत्य\nवद २==१; # असत्य\nवद २!=१; # सत्य\nवद २>=१; # सत्य\nवद २>=१; # सत्य\nवद २<=१; # असत्य\nवद २&&१; # सत्य\nवद २||१; # सत्य",
   ],
   [
     "suchi.ved",
-    '# Creating an सूचि\nमान क = [["आम",१ ],["संतरा",२],["सेब",३]];\nवद् क;\n\n# Adding elements to a सूचि\nमान सप्तर्षि = []; # रिक्त सूचि - Empty list\n\nमान सप्तर्षि[]= "Kashyapa";\nमान सप्तर्षि[]= "Atri";\nमान सप्तर्षि[]= "Vasistha";\nमान सप्तर्षि[]= "Vishwamitra";\nमान सप्तर्षि[]= "Bharadvaja";\nमान सप्तर्षि[]= "Gautama";\nमान सप्तर्षि[]= "Jamadagni";\n\nवद् सप्तर्षि;\nवद् कुल(सप्तर्षि);',
+    '# Creating an सूचि\nमान क = [["आम",१ ],["संतरा",२],["सेब",३]];\nवद क;\n\n# Adding elements to a सूचि\nमान सप्तर्षि = []; # रिक्त सूचि - Empty list\n\nमान सप्तर्षि[]= "Kashyapa";\nमान सप्तर्षि[]= "Atri";\nमान सप्तर्षि[]= "Vasistha";\nमान सप्तर्षि[]= "Vishwamitra";\nमान सप्तर्षि[]= "Bharadvaja";\nमान सप्तर्षि[]= "Gautama";\nमान सप्तर्षि[]= "Jamadagni";\n\nवद सप्तर्षि;\nवद कुल(सप्तर्षि);',
   ],
   [
     "sutra.ved",
-    "सूत्र योग(अ,ब){\n    फल अ+ब;\n}\n\nवद् योग(११,२२); # it will print ३३ ",
+    "सूत्र योग(अ,ब){\n    फल अ+ब;\n}\n\nवद योग(११,२२); # it will print ३३ ",
   ],
   [
     "viram.ved",
-    '# This will print only till ५, it will break after अ = ५\nचक्रम्(मान अ = ०; अ < १०; मान  अ = अ+ १)\n{\n  यदि (अ > ५){\n    विराम्;\n }\n  वद्  "अ = " + अ;\n}',
+    '# This will print only till ५, it will break after अ = ५\nचक्रम्(मान अ = ०; अ < १०; मान  अ = अ+ १)\n{\n  यदि (अ > ५){\n    विराम्;\n }\n  वद  "अ = " + अ;\n}',
   ],
   [
     "yadiath.ved",
-    'मान आयु = ३२;\nयदि (आयु == २५) {\n  वद् "वयं सम वयस्काः एव"; # We\'re the same age\n}\nअथ यदि (आयु > २५) {\n  वद् "वयं सम वयस्काः न एव"; # We\'re not the same age\n}\nअथ {\n  वद् "मम अपेक्षया कनिष्ठः वा"; # Younger to me\n}',
+    'मान आयु = ३२;\nयदि (आयु == २५) {\n  वद "वयं सम वयस्काः एव"; # We\'re the same age\n}\nअथ यदि (आयु > २५) {\n  वद "वयं सम वयस्काः न एव"; # We\'re not the same age\n}\nअथ {\n  वद "मम अपेक्षया कनिष्ठः वा"; # Younger to me\n}',
   ],
   [
     "nirdesa.ved",
-    "मान अ = ३;\n\nनिर्देश (अ) {\n    यद १:\n        वद् 'निर्देश १ अनुष्ठित'; # case 1 executed\n    यद २:\n        वद् 'निर्देश २ अनुष्ठित'; \n    यद ३:\n        वद् 'निर्देश ३ अनुष्ठित'; \n    यदभावे:\n        वद् 'यदभावे औत्सर्गिक निर्देश अनुष्ठित'; # In the absence of incident default case executed\n}",
+    "मान अ = ३;\n\nनिर्देश (अ) {\n    यद १:\n        वद 'निर्देश १ अनुष्ठित'; # case 1 executed\n    यद २:\n        वद 'निर्देश २ अनुष्ठित'; \n    यद ३:\n        वद 'निर्देश ३ अनुष्ठित'; \n    यदभावे:\n        वद 'यदभावे औत्सर्गिक निर्देश अनुष्ठित'; # In the absence of incident default case executed\n}",
   ],
   [
     "chakram.ved",
-    'चक्रम्(मान अ = ०; अ < १०; मान  अ = अ+ १)\n{\n  # चक्रम् takes in 3 parameters initial value, and condition, and a step size\n  वद्  "अ = " + अ;\n} \n# This will print ० to ९',
+    'चक्रम्(मान अ = ०; अ < १०; मान  अ = अ+ १)\n{\n  # चक्रम् takes in 3 parameters initial value, and condition, and a step size\n  वद  "अ = " + अ;\n} \n# This will print ० to ९',
   ],
   [
     "simpleinterest",
-    'सूत्र मिश्रधन(म,द,स){\n    फल म+(म*द*स/१००);\n   }\nमान मूलधन = १०००;\nमान दर = ८;\nमान समय = ५; \n\nवद् "मिश्रधन = \n"+मिश्रधन(मूलधन,दर,समय);',
+    'सूत्र मिश्रधन(म,द,स){\n    फल म+(म*द*स/१००);\n   }\nमान मूलधन = १०००;\nमान दर = ८;\nमान समय = ५; \n\nवद "मिश्रधन ="+मिश्रधन(मूलधन,दर,समय);',
   ],
 ]);
 for (let name of examples.keys()) {
